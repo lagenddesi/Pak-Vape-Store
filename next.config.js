2ca3f16963dcfac755ab1a2821a7277df93660c1
lagenddesi <lagenddesi76@gmail.com>
2026-01-16 18:30:14 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  /* Agar aap images use kar rahe hain to ye domains allow karna behtar hai */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 }
+
 module.exports = nextConfig
